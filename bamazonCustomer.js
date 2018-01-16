de85b19bfdb13fe50bd77adaa491759d99e1ddd2
Philipp Wickart <phwickart@gmail.com>
2018-01-16 16:25:33 -0500

    update

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -7,97 +7,194 @@ var connection = mysql.createConnection({
   port: 3306,
   user: "root",
   password: "",
-  database: "bamazon"
+  database: "bamazon_1"
 });
 
 connection.connect(function(err) {
   if (err) throw err;
   // run the start function after the connection is made to prompt the user
-  start();
 });
 
 function checkId() {
-  // query the database for all ID's
-  connection.query("SELECT * FROM products", function(err, results) {
-    if (err) throw err;
-    // once you have the ID's, prompt the user to enter the ID of the product that they'd like to buy
-    inquirer
-      .prompt([
+    inquirer.prompt([
         {
           name: "productId",
-          type: "confirm",
+          type: "input",
           message: "Please enter the ID of the product that you would like to buy!"
         },
       ])
       .then(function(answer) {
+        var query = `SELECT stock_quantity FROM products WHERE item_id=${answer.productId}`;
+            connection.query(query, function(err, results){  
+            if (err) {
+              return console.log("Error: ", err);
+            }
+            var stock_quantity = results[0].stock_quantity;
+            var selectedProduct = answer.productID;
 
-	      	id: function() {		
-	            var idArray = [];
-	            for (var i = 0; i < results.length; i++) {
-	              idArray.push(results[i].item_id);
-	            }
-	            return idArray;
-
-	        	var chosenID = result.productId;
-
-		        if(chosenID === idArray[i]) {
-		        	inquirer
-		        		.prompt([
-		        			{
-		        				name: "quantity",
-		        				type: "confirm",
-		        				message: "Please enter the amount of units that you would like to buy!"
-		        			};
-		        		])
-		        		.then(function(answer){
-		        			readProducts();
-		        		})
-		        }
-		        else {
-		        	console.log("Wrong ID. Please re-enter ID or call customer support for help");
-		        	checkId();
-		        };
-	    	};
-	    });
-  });
+            quantity(stock_quantity);
+            
+            
+            }) 
+      });
 };
 
-function readProducts() {
-  connection.query("SELECT * FROM products", function(err, res) {
-    if (err) throw err;
+function quantity(stock_quantity) {
+  inquirer.prompt([
+    {name: "quantity",
+    type: "input",
+    message: "Please enter the quantity"
+    },
+  ])
+    .then(function(answer){
+      if(answer.quantity > stock_quantity) {
+        console.log("Sorry, insufficient stock")
+      } else {
+          console.log("Thanks");
+          var newQuantity = stock_quantity - answer.quantity
+          console.log(newQuantity);
+          updateProduct()
+        };
+      });
+  };
 
-    if (result.stock_quantity >= parseInt(answer.quantity)) {
-    	updateProduct();
-    }
-    else {
-    	console.log("Insufficient quantity");
-    	console.log("Please chose another product");
-    	checkId();
-    }    
-  });
-}
 
-function updateProduct() {
-  console.log("Updating all quantities...\n");
+// function updateProduct() {
+//   console.log("Updating all quantities\n");
+//   var query = "UPDATE products SET ? WHERE ?";
+//   UPDATE products SET stock_quantity = '[updated-value]' WHERE item_id = ;
+
+//   connection.query(query, { stock_quantity: answer.artist }, function(err, results){
+//       printInfo(results);
+    
+//     function(error) {
+//               if (error) throw err;
+//               var totalCost = (parseInt(answer.quantity) * result.price);
+//               console.log("Order placed successfully!");
+//               console.log("Total Cost: " + totalCost);
+//               start();
+//             }
+//   );
+
+function updateProduct(newQuantity, selectedProduct, stock_quantity) {
+  console.log("Updating all quantities\n");
   var query = connection.query(
     "UPDATE products SET ? WHERE ?",
     [
       {
-        stock_quantity: (result.stock_quantity - answer.quantity)
+        stock_quantity: newQuantity
       },
+      {
+        item_id: selectedProduct
+      }
     ],
-    function(error) {
-              if (error) throw err;
-              var totalCost = (parseInt(answer.quantity) * result.price);
-              console.log("Order placed successfully!");
-              console.log("Total Cost: " + totalCost);
-              start();
-            }
+    function(err, res) {
+      console.log(stock_quantity);
+    }
   );
 
   // logs the actual query being run
-  console.log(query.sql);
+  //console.log(query.sql);
 }
 
-// show total costs for customer
+
+
+checkId();
+//quantity();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//         // query the database for all ID's
+//   connection.query("SELECT * FROM products", function(err, results) {
+//     if (err) throw err;
+//     // once you have the ID's, promp
+
+
+//         checkId();
+// 	      	id: function() {		
+// 	            var idArray = [];
+// 	            for (var i = 0; i < results.length; i++) {
+// 	              idArray.push(results[i].item_id);
+// 	            }
+// 	            return idArray;
+
+// 	        	var chosenID = result.productId;
+
+// 		        if(chosenID === idArray[i]) {
+// 		        	inquirer
+// 		        		.prompt([
+// 		        			{
+// 		        				name: "quantity",
+// 		        				type: "confirm",
+// 		        				message: "Please enter the amount of units that you would like to buy!"
+// 		        			};
+// 		        		])
+// 		        		.then(function(answer){
+// 		        			readProducts();
+// 		        		})
+// 		        }
+// 		        else {
+// 		        	console.log("Wrong ID. Please re-enter ID or call customer support for help");
+// 		        	checkId();
+// 		        };
+// 	    	};
+// 	    });
+//   });
+// };
+
+// function readProducts() {
+//   connection.query("SELECT * FROM products", function(err, res) {
+//     if (err) throw err;
+
+//     if (result.stock_quantity >= parseInt(answer.quantity)) {
+//     	updateProduct();
+//     }
+//     else {
+//     	console.log("Insufficient quantity");
+//     	console.log("Please chose another product");
+//     	checkId();
+//     }    
+//   });
+// }
+
+// function updateProduct() {
+//   console.log("Updating all quantities...\n");
+//   var query = connection.query(
+//     "UPDATE products SET ? WHERE ?",
+//     [
+//       {
+//         stock_quantity: (result.stock_quantity - answer.quantity)
+//       },
+//     ],
+//     function(error) {
+//               if (error) throw err;
+//               var totalCost = (parseInt(answer.quantity) * result.price);
+//               console.log("Order placed successfully!");
+//               console.log("Total Cost: " + totalCost);
+//               start();
+//             }
+//   );
+
+//   // logs the actual query being run
+//   console.log(query.sql);
+// }
+// checkId();
+// readProducts();
+// updateProduct();
+// // show total costs for customer
 
--- homework.js
+++ homework.js
@@ -1,44 +1,63 @@
-CREATE DATABASE bamaszon;
 
--- Makes it so all of the following code will affect bamaszon --
-USE bamaszon;
-
--- Creates the table "people" within bamaszon --
-CREATE TABLE products (
+DROP DATABASE IF EXISTS bamazon;
+CREATE DATABASE bamazon;
+USE bamazon;
+CREATE TABLE products(
   item_id INTEGER(11) AUTO_INCREMENT NOT NULL,
   product_name VARCHAR(30) NOT NULL,
   department_name VARCHAR(30) NOT NULL,
   price INTEGER(10),
-  stock_quantity INTEGER(10),
+  stock_quantity INTEGER(10)
   );
 
 
-INSERT INTO products (product_name, department_name, price stock_quantity)
-VALUES ("MacBook Air", "Electronics", 999, 100);
 
-INSERT INTO products (product_name, department_name, price stock_quantity)
-VALUES ("iPhone", "Electronics", 400, 50);
+DROP DATABASE IF EXISTS greatbuy_db;
+CREATE DATABASE greatbuy_db;
+USE greatbuy_db;
 
-INSERT INTO products (product_name, department_name, price stock_quantity)
-VALUES ("iWatch", "Electronics", 299, 250);
+CREATE TABLE products(
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  name VARCHAR(50),
+  minBid INTEGER(10),
+  category VARCHAR(50),
+  PRIMARY KEY (id)
+);
 
-INSERT INTO products (product_name, department_name, price stock_quantity)
+
+INSERT INTO products (name, minBid, category)
+VALUES ("MacBook Air", 500, "Electronics");
+
+INSERT INTO products (name, minBid, category)
+VALUES ("iPhone X", 400, "Electronics");
+
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("MacBook Air", "Computers", 999, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("iPhone", "Computers", 400, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("iWatch", "Computers", 299, 250);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
 VALUES ("Bose Headphones", "Electronics", 350, 75);
 
-INSERT INTO products (product_name, department_name, price stock_quantity)
-VALUES ("Seagate External Harddisk", "Electronics", 1500, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Seagate External Harddisk", "Computers", 1500, 100);
 
-INSERT INTO products (product_name, department_name, price stock_quantity)
-VALUES ("iPad mini", "Electronics", 400, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("iPad mini", "Computers", 400, 100);
 
-INSERT INTO products (product_name, department_name, price stock_quantity)
-VALUES ("iPad Keyboard", "Electronics", 100, 200);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("iPad Keyboard", "Computers", 100, 200);
 
-INSERT INTO products (product_name, department_name, price stock_quantity)
-VALUES ("Sonos Speakers", "Electronics", 350, 20);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Sonos Speakers", "Computers", 350, 20);
 
-INSERT INTO products (product_name, department_name, price stock_quantity)
-VALUES ("Drone", "Electronics", 99, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Drone", "Toys", 99, 100);
 
-INSERT INTO products (product_name, department_name, price stock_quantity)
-VALUES ("Canon Camero", "Electronics", 1999, 100);
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Canon Camera", "Photo", 1999, 100);
